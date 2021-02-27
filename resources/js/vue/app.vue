bccd30c8288749939cc56d1ae26fefee29e566be
denizyoldas <36186348+denizyoldas@users.noreply.github.com>
2021-02-27 23:33:05 +0300

    first commit

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="todoListContainer">
+    <div class="hedding">
+      <h2 id="title">Todo List</h2>
+      <add-item-form />
+    </div>
+    <list-view />
+  </div>
+</template>
+
+
+<script>
+import addItemForm from "./addItemForm.vue";
+import ListView from "./listView.vue";
+export default {
+  components: { addItemForm, ListView },
+};
+</script>
+
+<style scoped>
+.todoListContainer {
+  width: 350px;
+  margin: auto;
+}
+
+.hedding {
+  background: #e6e6e6e6;
+  padding: 10px;
+}
+
+#title {
+  text-align: center;
+}
+</style>
---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        listView
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
